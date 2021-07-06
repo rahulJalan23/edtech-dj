8948381c8b8204268b2cedd86442aee3f7b8c0b8
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2021-07-06 11:45:58 +0530

    registered models to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Textbook)
+admin.site.register(Subject)
+admin.site.register(Course)
+admin.site.register(Branch)
 
 # Register your models here.
